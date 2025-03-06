1a6d7a60897d1c363760c84f35ece3b8db235a48
Kaibo-He <kaibo.he@mail.utoronto.ca>
2025-03-06 01:31:43 -0500

    --

--- frontend/src/pages/components/ImageDetail.js
+++ frontend/src/pages/components/ImageDetail.js
@@ -5,15 +5,33 @@ import '../../style.css'; // 样式文件
 const ImageDetail = ({ content }) => {
     const conSrc = `${config.backendUrl}${content}`;
 
-    return <img 
-      src={conSrc} 
-      alt="Protected" 
-      draggable={false} 
-      className='detail-img' 
-      onContextMenu={(e) => e.preventDefault()} 
-      onTouchStart={(e) => e.preventDefault()}
-      onTouchEnd={(e) => e.preventDefault()}
-    />;
+    return (
+      <div style={{ position: 'relative', display: 'inline-block' }}>
+        <img 
+          src={conSrc} 
+          alt="Protected" 
+          draggable={false} 
+          className='detail-img' 
+          style={{
+            display: 'block',
+            pointerEvents: 'none', // 禁止交互
+            userSelect: 'none'
+          }}
+        />
+        <div 
+          style={{
+            position: 'absolute',
+            top: 0, left: 0, width: '100%', height: '100%',
+            background: 'transparent',
+            zIndex: 10
+          }}
+          className='img-protector'
+          onContextMenu={(e) => e.preventDefault()} 
+          onTouchStart={(e) => e.preventDefault()} 
+          onTouchEnd={(e) => e.preventDefault()} 
+        />
+    </div> 
+    );
   };
   
 export default ImageDetail;
